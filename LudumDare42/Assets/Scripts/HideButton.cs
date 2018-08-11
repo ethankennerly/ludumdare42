8bec0233499669600fab9e392fba1f453f2a57f0
Ethan Kennerly <ethan.kennerly@glu.com>
2018-08-10 19:42:31 -0700

    Hide Button: Hides object.

---
+++ LudumDare42/Assets/Scripts/HideButton.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FineGameDesign.LudumDare42
+{
+    public sealed class HideButton : MonoBehaviour
+    {
+        [SerializeField]
+        private Button m_HideButton;
+
+        [SerializeField]
+        private GameObject m_HideObject;
+
+        private void OnEnable()
+        {
+            m_HideButton.onClick.AddListener(HideObject);
+        }
+
+        private void OnDisable()
+        {
+            m_HideButton.onClick.RemoveListener(HideObject);
+        }
+
+        private void HideObject()
+        {
+            m_HideObject.SetActive(false);
+        }
+    }
+}
