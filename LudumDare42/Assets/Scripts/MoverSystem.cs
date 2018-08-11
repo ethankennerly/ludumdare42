a7d1f435580424dfa4285352d8e8e34b27371c77
Ethan Kennerly <ethan.kennerly@glu.com>
2018-08-11 12:24:25 -0700

    Mover System: Listens to pause system delta time.

--- LudumDare42/Assets/Scripts/MoverSystem.cs
+++ LudumDare42/Assets/Scripts/MoverSystem.cs
@@ -5,12 +5,11 @@ namespace FineGameDesign.Utils
 {
     /// <summary>
     /// 1. [ ] Mover System
-    ///     1. [ ] Is Paused.
-    ///     1. [ ] Speed (vector).
-    ///     1. [ ] Moving Objects.
+    ///     1. [x] Speed (vector).
+    ///     1. [x] Moving Objects.
     ///     1. [ ] Publishes On Position Changed.
-    ///         1. [ ] Camera and vehicle stay still.
-    ///         1. [ ] Objects in grid slide in direction of camera along z axis.
+    ///         1. [x] Camera and vehicle stay still.
+    ///         1. [x] Objects in grid slide in direction of camera along z axis.
     ///         - Otherwise, floating point numbers becomes less precise.
     ///         <http://davenewson.com/posts/2013/unity-coordinates-and-scales.html>
     ///     1. [ ] Set X Speed
@@ -18,18 +17,30 @@ namespace FineGameDesign.Utils
     [Serializable]
     public sealed class MoverSystem : ASingleton<MoverSystem>
     {
+        public static event Action<Vector3> onMoved;
+
         [SerializeField]
         private Vector3 m_Speed;
 
         [SerializeField]
         private Transform[] m_Transforms;
 
+        private Vector3 m_Position;
+
         public void Update(float deltaTime)
         {
             Vector3 step = deltaTime * m_Speed;
+            if (step == Vector3.zero)
+                return;
+
             foreach (Transform transform in m_Transforms)
             {
                 transform.position += step;
+            }
+
+            if (onMoved != null)
+            {
+                onMoved(step);
             }
         }
     }
--- LudumDare42/Assets/Scripts/MoverSystemView.cs
+++ LudumDare42/Assets/Scripts/MoverSystemView.cs
@@ -1,12 +1,36 @@
-using UnityEngine;
+using System;
 
 namespace FineGameDesign.Utils
 {
+    /// <summary>
+    /// Only updates when not paused.
+    /// </summary>
     public sealed class MoverSystemView : ASingletonView<MoverSystem>
     {
-        private void Update()
+        private Action<float> m_OnDeltaTime;
+
+        private void OnEnable()
         {
-            controller.Update(PauseSystem.instance.deltaTime);
+            AddListener();
+        }
+
+        private void OnDisable()
+        {
+            RemoveListener();
+        }
+
+        private void AddListener()
+        {
+            if (m_OnDeltaTime == null)
+                m_OnDeltaTime = controller.Update;
+
+            PauseSystem.onDeltaTime -= m_OnDeltaTime;
+            PauseSystem.onDeltaTime += m_OnDeltaTime;
+        }
+
+        private void RemoveListener()
+        {
+            PauseSystem.onDeltaTime -= m_OnDeltaTime;
         }
     }
 }
